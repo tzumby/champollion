8b248321fa031030361f8c6324bf0e33175ca27c
Razvan <tzumby@gmail.com>
2015-05-01 17:09:47 -0400

    Removed custom i18n backend, added i18n-active_record gem as dependency

--- app/models/champollion/translation.rb
+++ app/models/champollion/translation.rb
@@ -1,4 +1,6 @@
 module Champollion
   class Translation < ActiveRecord::Base
+    self.table_name = "translations"
+
   end
 end
--- lib/champollion.rb
+++ lib/champollion.rb
@@ -1,6 +1,4 @@
 require "champollion/engine"
-require "champollion/i18n_backend"
-require "champollion/simple"
 
 module Champollion
 end
